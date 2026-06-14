de33159fdb3cd572281c9de2be723834018f33de
1902797c <1902797c@umich.mx>
2026-06-13 20:39:19 -0600

    Update APP_MC.py

--- APP_MC.py
+++ APP_MC.py
@@ -90,7 +90,7 @@ if tipo_col in ["Circular con espiral", "Circular con estribos circulares"]:
     st.sidebar.caption(f"ds = {ds:.2f} cm  |  ρs = {rho_s*100:.2f}%")
 
     if rho_s < RHO_MIN:
-    st.sidebar.error("⚠️ ρs < 1% (BAJO)")
+        st.sidebar.error("⚠️ ρs < 1% (BAJO)")
 
     if tipo == "Circular con espiral":
         st.sidebar.markdown("🔧 **Recomendación:**")
